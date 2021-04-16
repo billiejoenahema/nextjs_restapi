9de351dd0eded17f2da6d71a01a41ab981d15451
billiejoenahema <billiejoenahema@gmail.com>
2021-04-16 21:13:53 +0900

    fix: line

--- api/models.py
+++ api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Post(models.Model):
+
     title = models.CharField(max_length=50)
     content = models.CharField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -11,6 +12,7 @@ class Post(models.Model):
 
 
 class Task(models.Model):
+
     title = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
 
